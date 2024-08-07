9c174988d691766a5975a70fa57c82e9dad9bfba
Jennifer Mariano <131280055+Jenni201206@users.noreply.github.com>
2024-08-06 21:28:31 -0300

    Add files via upload

---
+++ atividades/atividade-05/script.py
@@ -0,0 +1,84 @@
+import os
+import sqlite3
+
+all_id = []
+
+script_dir = os.path.realpath(os.path.dirname(__file__))
+
+db_path = os.path.join(script_dir, 'tasks.db')
+
+conexao = sqlite3.connect(db_path)
+
+conexao.execute('''
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER PRIMARY KEY,
+        description TEXT,
+        completed INTEGER
+    );
+''')
+
+def create_task(description):
+    conexao.execute("INSERT INTO tasks (description, completed) VALUES (?, 0)", (description,))
+
+def list_tasks():
+    cursor = conexao.execute("SELECT id, description, completed FROM tasks")
+    tasks = cursor.fetchall()
+    
+    if not tasks:
+        print("Não possui tarefas disponíveis!")
+        return False
+    else:
+        for task in tasks:
+            print(f"ID: {task[0]}, Descrição: {task[1]}, concluída: {'Sim' if task[2] else 'Não'}")
+            all_id.append(task[0])
+        return True
+
+def mark_completed(task_id):
+    conexao.execute("UPDATE tasks SET completed = 1 WHERE id = ?", (task_id,))
+
+def delete_task(task_id):
+    conexao.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+
+while True:
+    print("""
+- - - - - - Lista de tarefas - - - - - -
+1) Criar uma tarefa
+2) Mostrar tarefas existentes
+3) Marcar uma tarefa como concluída
+4) Deletar uma tarefa
+5) Sair   
+""")
+    decisao = int(input("Escolha uma opção: "))
+
+    match decisao:
+        case 1:
+            input_tarefa = input("Qual a descrição da sua tarefa? ")
+            create_task(input_tarefa)
+        case 2:
+            list_tasks()
+        case 3:
+            if list_tasks():
+                while True:
+                    choose_task_to_complete = int(input("Qual o id da tarefa que deseja marcar como Concluída? "))
+                    if choose_task_to_complete in all_id:
+                        mark_completed(choose_task_to_complete)
+                        break
+                    else:
+                        print()
+                        print("ID inválido. Por favor, tente novamente.")
+                        print()
+        case 4:
+            if list_tasks():
+                while True:
+                    choose_task_to_delete = int(input("Digite o id da tarefa que deseja deletar: "))
+                    if choose_task_to_delete in all_id:
+                        delete_task(choose_task_to_delete)
+                        break
+                    else:
+                        print()
+                        print("ID inválido. Por favor, tente novamente.")
+                        print()
+        case 5:
+            break
+
+conexao.close()
